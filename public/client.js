fb7e57fdb9e486109479d9044c6a2eadc61f7014
Jesss-Jesss <jestinmosterd@gmail.com>
2026-02-18 16:11:15 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,35 +1,20 @@
 const socket = io();
 
-if (window.location.pathname === "/host") {
-
-    fetch("/gamecode")
-        .then(res => res.json())
-        .then(data => {
-            document.getElementById("gameCode").innerText = data.code;
-
-            const joinURL = window.location.origin + "/player";
-            document.getElementById("qrCode").src =
-                "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" + joinURL;
-        });
-
-    socket.on("updatePlayers", (players) => {
-        const list = document.getElementById("playerList");
-        list.innerHTML = "";
-        players.forEach(p => {
-            const li = document.createElement("li");
-            li.innerText = p.name;
-            list.appendChild(li);
-        });
-    });
-}
-
 function joinGame() {
-    const code = document.getElementById("codeInput").value;
-    const name = document.getElementById("nameInput").value;
+    const name = document.getElementById("name").value;
+    const code = document.getElementById("code").value;
 
-    socket.emit("joinGame", { code, name });
+    socket.emit("joinGame", { name, code });
 }
 
 socket.on("nameTaken", () => {
-    document.getElementById("status").innerText = "Naam is al bezet!";
+    document.getElementById("message").textContent = "Naam is al in gebruik!";
+});
+
+socket.on("invalidCode", () => {
+    document.getElementById("message").textContent = "Ongeldige game code!";
+});
+
+socket.on("updatePlayers", () => {
+    document.getElementById("message").textContent = "Je bent succesvol gejoined!";
 });
